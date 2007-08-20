b8a72424253ad3891674f017feadfa371a72f523
Matthew Jason Benson <mbenson@apache.org>
2007-08-20 22:27:43 +0000

    undo formatting changes
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/el/trunk@567859 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/el/VariableResolverImpl.java
+++ src/java/org/apache/commons/el/VariableResolverImpl.java
@@ -28,70 +28,96 @@ import javax.servlet.jsp.el.VariableResolver;
  * 
  * @author Nathan Abramson - Art Technology Group
  * @version $Change: 181177 $$DateTime: 2001/06/26 08:45:09 $$Author$
- */
-public class VariableResolverImpl implements VariableResolver {
-    // -------------------------------------
-    // Member variables
-    // -------------------------------------
+ **/
 
-    private PageContext mCtx;
+public class VariableResolverImpl
+  implements VariableResolver
+{
+  //-------------------------------------
+  // Member variables
+  //-------------------------------------
 
-    // -------------------------------------
-    /**
-     *
-     * Constructor
-     */
-    public VariableResolverImpl(PageContext pCtx) {
-        mCtx = pCtx;
+  private PageContext mCtx;
+
+  //-------------------------------------
+  /**
+   *
+   * Constructor
+   **/
+  public VariableResolverImpl (PageContext pCtx)
+  {
+    mCtx = pCtx;
+  }
+  
+  //-------------------------------------
+  /**
+   *
+   * Resolves the specified variable within the given context.
+   * Returns null if the variable is not found.
+   **/
+  public Object resolveVariable (String pName)
+    throws ELException
+  {
+    // Check for implicit objects
+    if ("pageContext".equals (pName)) {
+      return mCtx;
+    }
+    else if ("pageScope".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getPageScopeMap ();
+    }
+    else if ("requestScope".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getRequestScopeMap ();
+    }
+    else if ("sessionScope".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getSessionScopeMap ();
+    }
+    else if ("applicationScope".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getApplicationScopeMap ();
+    }
+    else if ("param".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getParamMap ();
+    }
+    else if ("paramValues".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getParamsMap ();
+    }
+    else if ("header".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getHeaderMap ();
+    }
+    else if ("headerValues".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getHeadersMap ();
+    }
+    else if ("initParam".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getInitParamMap ();
+    }
+    else if ("cookie".equals (pName)) {
+      return ImplicitObjects.
+	getImplicitObjects (mCtx).
+	getCookieMap ();
     }
 
-    // -------------------------------------
-    /**
-     *
-     * Resolves the specified variable within the given context.
-     * Returns null if the variable is not found.
-     */
-    public Object resolveVariable(String pName) throws ELException {
-        // Check for implicit objects
-        if ("pageContext".equals(pName)) {
-            return mCtx;
-        }
-        if ("pageScope".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getPageScopeMap();
-        }
-        if ("requestScope".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx)
-                    .getRequestScopeMap();
-        }
-        if ("sessionScope".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx)
-                    .getSessionScopeMap();
-        }
-        if ("applicationScope".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx)
-                    .getApplicationScopeMap();
-        }
-        if ("param".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getParamMap();
-        }
-        if ("paramValues".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getParamsMap();
-        }
-        if ("header".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getHeaderMap();
-        }
-        if ("headerValues".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getHeadersMap();
-        }
-        if ("initParam".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getInitParamMap();
-        }
-        if ("cookie".equals(pName)) {
-            return ImplicitObjects.getImplicitObjects(mCtx).getCookieMap();
-        }
-        // Otherwise, just look it up in the page context
-        return mCtx.findAttribute(pName);
+    // Otherwise, just look it up in the page context
+    else {
+      return mCtx.findAttribute (pName);
     }
-
-    // -------------------------------------
+  }
+					
+  //-------------------------------------
 }
